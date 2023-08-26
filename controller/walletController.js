a35494348029b495c48d141325175ed2d2997267
vasaniyakush <vasaniyakush@gmail.com>
2023-08-26 09:39:31 +0530

    ok

--- controller/walletController.js
+++ controller/walletController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const walletServices = require("../services/walletServices");
-const { Wallet } = require("../models");
+const { Wallet, WithdrawRequest } = require("../models");
+const { ApiBadRequestError } = require("../errors");
 
 
 
@@ -9,7 +10,25 @@ exports.getWallet = asyncHandler(async(req,res)=>{
     res.status(200).json({data:rslt})
 })
 exports.withdrawMoneyRequest = asyncHandler( async(req,res)=>{
+    const {amount,details} = req.body;
+
     const wallet = await Wallet.findOne({
+        where:{
+
+            userId:req.user.uid
+        }
         
     })
+    if(wallet.amount < amount){
+        throw new ApiBadRequestError("Insufficient Balance")
+    }
+
+    const request = await WithdrawRequest.create({
+        userId:req.user.uid,
+        amount,
+        details,
+        status:"pending"
+    })
+    res.status(200).json({status:200,message:"Request generated to admin successfully",data:request})
+
 })
